541ef0f4e048ba8e8389483cf0fa8baef21e94a9
DNQ <dnq.httt@gmail.com>
2022-01-01 00:24:58 +0700

    setup source

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+	root: true,
+	extends: '@react-native-community',
+	rules: {
+		'react-hooks/exhaustive-deps': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'no-shadow': 'off',
+		'prettier/prettier': 'off',
+		'dot-notation': 'off',
+		curly: 'off',
+	},
 };
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 /**
  * @format
  */
+import {Platform, LogBox} from 'react-native';
+import './shim';
+
+if (Platform.OS !== 'web') {
+	require('react-native-get-random-values');
+	LogBox.ignoreLogs([
+		"Warning: The provided value 'ms-stream' is not a valid 'responseType'.",
+		"Warning: The provided value 'moz-chunked-arraybuffer' is not a valid 'responseType'.",
+	]);
+}
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
