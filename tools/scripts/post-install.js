919a861a88d996552b05522e8bd739d067cb21cb
DNQ <dnq.httt@gmail.com>
2022-01-02 00:10:34 +0700

    Update post-install.js

--- tools/scripts/post-install.js
+++ tools/scripts/post-install.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 
-if (
-	fs.existsSync(
-		'node_modules/@walletconnect/react-native-dapp/node_modules/react-native-svg',
-	)
-) {
-	fs.rmdirSync(
-		'node_modules/@walletconnect/react-native-dapp/node_modules/react-native-svg',
-	);
-}
+const mustRemovedDirs = [
+	'node_modules/@walletconnect/react-native-dapp/node_modules/react-native-svg',
+];
+
+mustRemovedDirs.forEach(dir => {
+	console.log('post-install', 'process-must-removed-dir', dir);
+	if (fs.existsSync(dir)) {
+		fs.rmdirSync(dir);
+		console.log('removed ', dir);
+	}
+});
